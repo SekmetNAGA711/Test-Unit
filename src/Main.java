015984984bd28c643622933cd6ac3e5a16ca6871
AbbyANG <bena.ao310@gmail.com>
2023-12-01 18:01:07 -0500

    test

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] intArr = new int[10];
+        intArr[0] = 45;
+        intArr[1] = 865;
+        intArr[2] = 23;
+        intArr[3] = 6;
+        intArr[4] = 368;
+        intArr[5] = 9032;
+        intArr[6] = 557;
+        intArr[7] = 226;
+        intArr[8] = 834564;
+        intArr[9] = 34001;
+
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println(intArr[i]);
+        }
+    }
+
+
+}
